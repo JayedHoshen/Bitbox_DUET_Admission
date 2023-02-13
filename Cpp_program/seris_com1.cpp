0b293f1f45aabb72ae31766e87ccdd195aa0f87a
jayedhoshen <jayedkn64@gmail.com>
2023-02-13 16:07:21 +0600

    update

---
+++ Cpp_program/seris_com1.cpp
@@ -0,0 +1,87 @@
+// 1-2*2+3*3-4*4...............n*n = ?
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, i, flag = 1;
+    double sum = 0;
+
+    cout <<"Enter value of n = ";
+    cin >> n;
+
+    for (i = 1; i<=n; i+=1){
+        sum += (flag*i*i);
+        flag*=-1;
+    }
+    cout << "Summition is = " << sum;
+
+    return 0;
+}
+
+
+
+/*
+// 1-1/2+1/3-1/4...............1/n = ?
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, i, flag = 1;
+    double sum = 0;
+
+    cout <<"Enter value of n = ";
+    cin >> n;
+
+    for (i = 1; i<=n; i+=1){
+        sum += (flag*1.0/i);
+        flag*=-1;
+    }
+    cout << "Summition is = " << sum;
+
+    return 0;
+}
+
+
+// 1-9+25-49...............n*n = ?
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, i, flag = 1, sum = 0;
+
+    cout <<"Enter value of n = ";
+    cin >> n;
+
+    for (i = 1; i<=n; i+=2){
+        sum += flag*i*i;
+        flag*=-1;
+    }
+    cout << "Summition is = " << sum;
+
+    return 0;
+}
+
+// 1-2+6-24...............n! = ?
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, i, fact = 1, flag = 1, sum = 0;
+
+    cout <<"Enter value of n = ";
+    cin >> n;
+
+    for (i = 1; i<=n; i+=1){
+        fact *= i;
+        sum += flag*fact;
+        flag*=-1;
+    }
+    cout << "Summition is = " << sum;
+
+    return 0;
+}
+*/
